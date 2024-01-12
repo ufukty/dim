75e3bdaa546b88b69be6012fa0d2d31d6017b47e
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-01-12 11:14:56 +0300

    fires focus event on EditorDecorator on launch

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,42 @@ var decorators: Map<vscode.TextEditor, ed.EditorDecorator>;
 var logger: vscode.OutputChannel;
 var activeEditor: vscode.TextEditor;
 
+function onDidChangeActiveTextEditor(editor: vscode.TextEditor | undefined): any {
+    if (editor === undefined || editor === activeEditor) return;
+
+    var d = decorators.get(activeEditor);
+    if (d) {
+        d.blur();
+    }
+
+    // first register change, then return; to ignore onDidChangeTextDocument that will be fired on Output panel
+    activeEditor = editor;
+    if (activeEditor.document.uri.scheme !== "file") return;
+
+    var d = decorators.get(editor);
+    if (d === undefined) {
+        d = new ed.EditorDecorator(editor, logger);
+        decorators.set(editor, d);
+    }
+    d.focus();
+}
+
+function onDidChangeTextDocument(event: vscode.TextDocumentChangeEvent): any {
+    if (event.document.uri.path !== activeEditor.document.uri.path || event.document.uri.scheme !== "file") return;
+
+    var d = decorators.get(activeEditor);
+    if (d) {
+        d.contentChange();
+    }
+
+    // const activeEditor = vscode.window.activeTextEditor;
+    // if (activeEditor === undefined || event.document !== activeEditor.document) return;
+    // logger.appendLine("onDidChangeTextDocument: " + event.document.uri.path.toString());
+    // editorDecorator.schedule(activeEditor);
+
+    // console.log(event.document, event.reason);
+}
+
 export function activate(context: vscode.ExtensionContext) {
     logger = vscode.window.createOutputChannel("dim");
     logger.appendLine("init");
@@ -12,45 +48,16 @@ export function activate(context: vscode.ExtensionContext) {
     decorators = new Map();
 
     vscode.window.onDidChangeActiveTextEditor((editor) => {
-        if (editor === undefined || editor === activeEditor) return;
-
-        var d = decorators.get(activeEditor);
-        if (d) {
-            d.blur();
-        }
-
-        // first register change, then return; to ignore onDidChangeTextDocument that will be fired on Output panel
-        activeEditor = editor;
-        if (activeEditor.document.uri.scheme !== "file") return;
-
-        var d = decorators.get(editor);
-        if (d === undefined) {
-            d = new ed.EditorDecorator(editor, logger);
-            decorators.set(editor, d);
-        }
-        d.focus();
-    });
-
-    vscode.workspace.onDidChangeTextDocument((event) => {
-        if (event.document.uri.path !== activeEditor.document.uri.path || event.document.uri.scheme !== "file") return;
-        
-        var d = decorators.get(activeEditor);
-        if (d) {
-            d.contentChange();
-        }
-
-        // const activeEditor = vscode.window.activeTextEditor;
-        // if (activeEditor === undefined || event.document !== activeEditor.document) return;
-        // logger.appendLine("onDidChangeTextDocument: " + event.document.uri.path.toString());
-        // editorDecorator.schedule(activeEditor);
-
-        // console.log(event.document, event.reason);
+        onDidChangeActiveTextEditor(editor);
     });
 
     if (vscode.window.activeTextEditor) {
-        // logger.appendLine("startup: " + vscode.window.activeTextEditor.document.uri.path.toString());
-        // editorDecorator.schedule(vscode.window.activeTextEditor);
+        onDidChangeActiveTextEditor(vscode.window.activeTextEditor);
     }
+
+    vscode.workspace.onDidChangeTextDocument((event) => {
+        onDidChangeTextDocument(event);
+    });
 }
 
 export function deactivate() {
